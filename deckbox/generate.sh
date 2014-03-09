92ee42d9e2db00bd9bc7bc315ce54a35028908b9
Cylindric <mark@hanfordonline.co.uk>
2014-03-09 10:48:27 +0000

    Force circle resolution and jitter for export.

--- deckbox/generate.sh
+++ deckbox/generate.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-openscad -D part=1 -o output/deck_box-1.stl deck_box.scad
-openscad -D part=2 -o output/deck_box-2.stl deck_box.scad
-openscad -D part=3 -o output/deck_box-3.stl deck_box.scad
-openscad -D part=4 -o output/deck_box-4.stl deck_box.scad
-openscad -D part=5 -o output/deck_box-5.stl deck_box.scad
-openscad -D part=6 -o output/deck_box-6.stl deck_box.scad
-openscad -D part=7 -o output/deck_box-7.stl deck_box.scad
-openscad -D part=8 -o output/deck_box-8.stl deck_box.scad
+openscad -D part=1 -D circleRes=120 -D j=0.1 -o output/deck_box-1.stl deck_box.scad
+openscad -D part=2 -D circleRes=120 -D j=0.1 -o output/deck_box-2.stl deck_box.scad
+openscad -D part=3 -D circleRes=120 -D j=0.1 -o output/deck_box-3.stl deck_box.scad
+openscad -D part=4 -D circleRes=120 -D j=0.1 -o output/deck_box-4.stl deck_box.scad
+openscad -D part=5 -D circleRes=120 -D j=0.1 -o output/deck_box-5.stl deck_box.scad
+openscad -D part=6 -D circleRes=120 -D j=0.1 -o output/deck_box-6.stl deck_box.scad
+openscad -D part=7 -D circleRes=120 -D j=0.1 -o output/deck_box-7.stl deck_box.scad
+openscad -D part=8 -D circleRes=120 -D j=0.1 -o output/deck_box-8.stl deck_box.scad
 
